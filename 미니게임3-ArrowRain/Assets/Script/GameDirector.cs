eabd9008b1d8987f110b04193c370488f426a0b8
Parksolmi <69065439+Parksolmi@users.noreply.github.com>
2021-09-08 12:05:56 +0900

    실습-미니게임

---
+++ 미니게임3-ArrowRain/Assets/Script/GameDirector.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameDirector : MonoBehaviour
+{
+    GameObject hpGage;
+
+    void Start()
+    {
+        this.hpGage = GameObject.Find("hpGuage");
+    }
+
+    public void DecreaseHP()
+    {
+        hpGage.GetComponent<Image>().fillAmount -= 0.1f;
+    }
+
+    void Update()
+    {
+        
+    }
+}
